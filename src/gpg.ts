50298055c5a5ab15ad419e2e695089afc8e1da5c
Andrei Horodniceanu <a.horodniceanu@proton.me>
2025-10-05 21:44:46 +0300

    try removing posix path stuff
    Signed-off-by: Andrei Horodniceanu <a.horodniceanu@proton.me>

--- src/gpg.ts
+++ src/gpg.ts
@@ -6,8 +6,8 @@ export async function verify(file_path: string, sig_url: string) {
     let keyring = await tc.downloadTool("https://dlang.org/d-keyring.gpg");
     let sig_path = await tc.downloadTool(sig_url);
     // hack to workaround gpg on windows interaction with paths
-    keyring = core.toPosixPath(keyring);
-    sig_path = core.toPosixPath(sig_path);
+    //keyring = core.toPosixPath(keyring);
+    //sig_path = core.toPosixPath(sig_path);
     await exec.exec(
 	'gpg',
         [ '--lock-never', '--verify', '--keyring', keyring, '--no-default-keyring',
